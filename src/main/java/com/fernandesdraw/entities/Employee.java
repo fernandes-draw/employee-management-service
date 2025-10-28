b12605ef531bd853062576d800e3c6b89344e9c6
fernandes-draw <fernandes_draw@hotmail.com>
2025-10-28 16:29:56 -0300

    first commit

---
+++ src/main/java/com/fernandesdraw/entities/Employee.java
@@ -0,0 +1,22 @@
+package com.fernandesdraw.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @JsonIgnore
+    private String password;
+
+}
